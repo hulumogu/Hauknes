aca2395d873365b41280bde8585309e072d301b3
Morten Hauknes <morten@hauknes.biz>
2023-10-01 20:37:24 +0200

    New version with sorted list with lowes pricest

--- nordpool_fetchprices.py
+++ nordpool_fetchprices.py
@@ -9,21 +9,41 @@ prices_spot = elspot.Prices()
 
 # Fetch hourly Elspot prices for Kr.sand and print the resulting dictionary
 hourValues = prices_spot.hourly(areas=['Kr.sand'])['areas']['Kr.sand']['values']
-#pprint(hourValues)
-minPrice = 100000
-maxPrice = 0
+
+
+# sorted dictionary with lowest values first
+valueHourDictionary = {}
 for hourValue in hourValues:
    currentPrice = hourValue['value']
    currentTime=(hourValue['start'].hour) 
-   if currentPrice < minPrice:
-       minPrice = currentPrice
-       minTime = currentTime
-   if currentPrice > maxPrice:
-       maxPrice = currentPrice
-       maxTime  = currentTime
+   valueHourDictionary[currentPrice] = currentTime
+
+# sort hours
+myKeys = list(valueHourDictionary.keys())
+myKeys.sort()
+sorted_lowest_prices_first = {i: valueHourDictionary[i] for i in myKeys}
+
+for price, time in sorted_lowest_prices_first.items():
+   pprint('current price is ' + str(price) + ' starting at ' + str(time)) 
+
+
+
+
+# lowest and highest version
+# minPrice = 100000
+# maxPrice = 0
+# for hourValue in hourValues:
+#    currentPrice = hourValue['value']
+#    currentTime=(hourValue['start'].hour) 
+#    if currentPrice < minPrice:
+#        minPrice = currentPrice
+#        minTime = currentTime
+#    if currentPrice > maxPrice:
+#        maxPrice = currentPrice
+#        maxTime  = currentTime
     
-   pprint('current price is ' + str(currentPrice) + ' starting at ' + str(hourValue['start'].hour)) 
+#    pprint('current price is ' + str(currentPrice) + ' starting at ' + str(hourValue['start'].hour)) 
   
-pprint('Max price is ' + str(maxPrice) + ' starting at ' + str(maxTime)) 
-pprint('Min price is ' + str(minPrice) + ' starting at ' + str(minTime)) 
+# pprint('Max price is ' + str(maxPrice) + ' starting at ' + str(maxTime)) 
+# pprint('Min price is ' + str(minPrice) + ' starting at ' + str(minTime)) 
 
