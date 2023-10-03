b3eb3ea811a3e1bc25e4d787772adef9caef704e
Morten Hauknes <morten@hauknes.biz>
2023-10-03 21:11:51 +0200

    2 lowest subsequent result

--- nordpool_fetchprices.py
+++ nordpool_fetchprices.py
@@ -11,25 +11,25 @@ prices_spot = elspot.Prices()
 hourValues = prices_spot.hourly(areas=['Kr.sand'])['areas']['Kr.sand']['values']
 
 
-# sorted dictionary with lowest values first
-valueHourDictionary = {}
-for hourValue in hourValues:
-   currentPrice = hourValue['value']
-   currentTime=(hourValue['start'].hour) 
-   valueHourDictionary[currentPrice] = currentTime
+# # sorted dictionary with lowest values first
+# valueHourDictionary = {}
+# for hourValue in hourValues:
+#    currentPrice = hourValue['value']
+#    currentTime=(hourValue['start'].hour) 
+#    valueHourDictionary[currentPrice] = currentTime
 
-# sort hours
-myKeys = list(valueHourDictionary.keys())
-myKeys.sort()
-sorted_lowest_prices_first = {i: valueHourDictionary[i] for i in myKeys}
+# # sort hours
+# myKeys = list(valueHourDictionary.keys())
+# myKeys.sort()
+# sorted_lowest_prices_first = {i: valueHourDictionary[i] for i in myKeys}
 
-for price, time in sorted_lowest_prices_first.items():
-   pprint('current price is ' + str(price) + ' starting at ' + str(time)) 
-
-
+# for price, time in sorted_lowest_prices_first.items():
+#    pprint('current price is ' + str(price) + ' starting at ' + str(time)) 
 
 
-# lowest and highest version
+
+
+# #lowest and highest version
 # minPrice = 100000
 # maxPrice = 0
 # for hourValue in hourValues:
@@ -46,4 +46,34 @@ for price, time in sorted_lowest_prices_first.items():
   
 # pprint('Max price is ' + str(maxPrice) + ' starting at ' + str(maxTime)) 
 # pprint('Min price is ' + str(minPrice) + ' starting at ' + str(minTime)) 
+
+#lowest subsequent result
+sizeHourValues = len(hourValues) 
+if sizeHourValues > 2:   
+    hourValue1 = hourValues[0]
+    hourValue2 = hourValues[1]
+    minPrice1 = hourValue1['value'] 
+    minPrice2 = hourValue2['value']
+    minTime1 = hourValue1['start'].hour 
+    minTime2 = hourValue2['start'].hour
+
+    pprint('current price is ' + str(minPrice1) + ' starting at ' + str(minTime1)) 
+    pprint('current price is ' + str(minPrice2) + ' starting at ' + str(minTime2)) 
+
+    for index in range(2, sizeHourValues):
+        lastValue = hourValues[index-1]
+        currentValue = hourValues[index]
+        if minPrice1 > lastValue['value'] and minPrice2 > currentValue['value']:
+            minPrice1 = lastValue['value']
+            minPrice2 = currentValue['value']
+            minTime1 = lastValue['start'].hour 
+            minTime2 = currentValue['start'].hour 
+
+    
+        currentPrice = currentValue['value']
+        currentTime=(currentValue['start'].hour) 
+        pprint('current price is ' + str(currentPrice) + ' starting at ' + str(currentTime)) 
+  
+pprint('Min price1 is ' + str(minPrice1) + ' starting at ' + str(minTime1)) 
+pprint('Min price2 is ' + str(minPrice2) + ' starting at ' + str(minTime2)) 
 
